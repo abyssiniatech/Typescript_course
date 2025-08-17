d25ead9c2243e49cf7760aece63a8131cab92449
surafel <surafelmengist2015@gmail.com>
2025-08-17 17:59:03 +0300

    types in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,61 @@
-function greet(name: string): string {
-  return `Hello, ${name}!`;
+// types in 
+// 1 string 
+
+
+let name="suraele";
+let bro:string="Welocome to me";
+// by default if the variable declare like these typescript compiler know about
+const address ="Hello these is the best app"
+
+// numbers
+let num1=12;
+const num2:number=23;
+const num3:number=0;
+
+// boolean type
+let isMarried=true;
+// isMarried=12   // errors are happened 
+
+
+// Array type
+let nums=[1,2,3,4,5,6,7];  //the first method
+let numbers:number[] = [12,34,45,56,67]
+// you can redeclare the same values if the array is number only push number array
+
+
+// object type
+const obj={
+    name:"surafel",
+    age:23,
+    Country:"Ethiopia"
 }
 
-console.log(greet("Surafel"));
+// if declare different data types you can execute errors 
+    //   example obj.age="9"
+
+// another way to declare objects
+
+type obj1={
+    name:string,
+    age:number,
+}
+const obj1={
+    name:"surafel mengist",
+    age:28,
+}
+
+
+//  2 types allies in typescript
+type userTypes={
+    name:string,
+    age:number,
+    isMarried:boolean,
+}
+
+ function types(user:userTypes){
+    const info=console.log(`my name is ${name} and i am ${age} and  ${isMarried}`)
+ }
+ types({name:"surafel", age:34, isMarried:true})
+
+
+
